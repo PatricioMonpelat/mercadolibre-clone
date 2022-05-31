8c9c9e1966c7c34224f962dfb88cc7366ae254e0
Patricio Monpelat <patricio.monpelat@gmail.com>
2022-05-31 16:02:52 -0300

    components, layout , header, hero, product

---
+++ src/components/Header/styles.js
@@ -0,0 +1,7 @@
+import styled  from "styled-components";
+
+export const Container = styled.header`
+width: 100vw;
+background-color: var(--yellow);
+`
+export const Wrapper = styled.nav``
---
+++ src/components/Producto/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const Producto = () => {
+    return(
+        <div>
+            <h1>Producto</h1>
+        </div>
+    )
+}

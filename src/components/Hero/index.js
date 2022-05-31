5ca635746d1ff27c2e5434c12d2cf64dae90c743
Patricio Monpelat <patricio.monpelat@gmail.com>
2022-05-31 16:18:55 -0300

    header style

--- src/components/Hero/index.js
+++ src/components/Hero/index.js
@@ -1,9 +1,22 @@
 import React from "react";
+import {Container, Row} from "./styles";
 
 export const Hero = () => {
     return(
-        <div>
-            <h1>Hero</h1>
-        </div>
+        <Container>
+          <Row>
+            <a href="#" className="volver">Volver al Listado</a>
+            <a href="#">Computacion</a>
+            <a href="#">Accesorios para PC</a>
+            <a href="#">Microfonos</a>
+            <a href="#">Redragon</a>
+           
+          </Row>
+          <Row>
+              <a href="#">Compartir</a>
+              <a href="#">Vender una igual</a>
+          </Row>
+          
+        </Container>
     )
 }

c98496436c9849470e38fbe5aa1ca512a9b1b7e4
Patricio Monpelat <patricio.monpelat@gmail.com>
2022-05-31 21:04:25 -0300

    infovendedor

--- src/components/Producto/index.js
+++ src/components/Producto/index.js
@@ -1,9 +1,65 @@
 import React from "react";
+import { InfoVendedor } from "../InfoVendedor";
+import { Container, Panel, Column, Galeria, ProductoVenta, Description, Section} from "./styles";
 
 export const Producto = () => {
-    return(
-        <div>
-            <h1>Producto</h1>
-        </div>
+  return (
+    <Container>
+      <Panel>
+        <Column>
+          <Galeria>
+              <img src="https://http2.mlstatic.com/D_NQ_NP_811838-MLA41372451107_042020-O.webp" alt=""/>
+          </Galeria>
+          <Info/>
+        </Column>
+        <Column>
+            <ProductoVenta/>
+            <InfoVendedor/>
+            <Garantia/>
+        </Column>
+      </Panel>
+    </Container>
+  );
+};
+
+const Info = () => {
+    return (
+        <Description>
+            <h4>Descripcion</h4>
+            <p>
+            <span>AURICULARES RAZEN</span>
+            <br/><br/>
+            <span>LIGERO Y COMODO</span>
+            <br/><br/>
+            El diseño over-ear brinda una comodidad insuperable gracias a sus suaves almohadillas. Al mismo tiempo, su sonido envolvente del más alto nivel se convierte en el protagonista de la escena.
+            <br/><br/>
+            El diseño over-ear brinda una comodidad insuperable gracias a sus suaves almohadillas. Al mismo tiempo, su sonido envolvente del más alto nivel se convierte en el protagonista de la escena.
+            <br/><br/>
+            <span>PENSADO PARA VOS</span>
+            <br/><br/>
+            ¡Experimentá la adrenalina de sumergirte en la escena de otra manera! Tener auriculares específicos para jugar cambia completamente tu experiencia en cada partida. Con los Razer Kraken no te perdés ningún detalle y escuchás el audio tal y como fue diseñado por los creadores.
+            </p>
+        </Description>
+    )
+
+}
+
+const Garantia = () => {
+    return (
+        <Section>
+            <h5>Garantía</h5>
+            <div>
+                <span>
+                    <p className="title">Compra Protegida con Mercado Pago</p>
+                    <p className="description">Recibe el producto que esperabas o te devolvemos tu dinero</p>
+                </span>
+            
+                <span>
+                    <p className="title">Garantia del vendedor</p>
+                    <p className="description">Garantia del vendedor: 6 meses</p>
+                </span>
+                <a href="#">Conocer más sobre garantía</a>
+            </div>
+        </Section>
     )
 }
---
+++ src/components/Producto/styles.js
@@ -0,0 +1,92 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+display: flex;
+flex-direction: column;
+margin-top: 12px;
+`
+
+export const Panel = styled.div`
+background-color: var(--white);
+box-shadow: var(--panel);
+display: grid;
+grid-template-columns: 67fr 33fr;
+`
+
+export const Column = styled.div``
+
+export const Galeria = styled.div`
+display: flex;
+align-items: center;
+justify-content: center;
+height: 530px;
+
+>img{
+    height: 75%
+}
+`
+
+
+
+export const ProductoVenta = styled.div``
+
+export const InfoVendedor = styled.div``
+
+export const Description = styled.div`
+border-top: 1px solid var(--border);
+width: 88%;
+margin: 0 auto;
+padding: 40px 0;
+>h5{
+    color: var(--text1);
+    font-size: 24px;
+    margin-bottom: 32px;
+    line-height: 30px;
+}
+>p, span{
+    line-height: 20px
+    font-size: 20px;
+    color: var(--text3);
+}
+>hr{
+    border-top: 1px solid var(--text2);
+}
+`
+
+export const Section = styled.div`
+padding: 32px 16px;
+margin: 0 16px;
+border: 1px solid var(--border);
+border-bottom: none;
+>h5{
+    font-size: 18px;
+    margin-bottom: 28px;
+    color: var(--text1);
+    line-height: 22.5px;
+}
+>div{
+    display: flex;
+    flex-direction: column;
+    line-height: 18.9px;
+
+    >span{
+       margin-bottom: 24px;
+
+    }
+    .title{
+        font-size: 16px;
+        color: var(--text1);
+        line-height: 21.6px;
+        margin-bottom: 12px;
+    }
+    .description{
+        font-size: 14px;
+        color: var(--text2);
+    }
+    >a{
+        font-size: 14px;
+        color: var(--blue3);
+        text-decoration: none;
+    }
+}
+`
